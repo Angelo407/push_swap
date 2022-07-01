7d50923157a62b532ed61b007a144ce9edc63155
Angelo <angelo@MacBook-Pro-de-Angelo.local>
2022-07-01 14:04:20 +0200

    ft_sa fonctionnelle

--- main.c
+++ main.c
@@ -1,27 +1,22 @@
 # include "push_swap.h"
 
-
-
-
-
+// 9, 4, 5, 1, 3
 
 int main(int argc, char **argv)
 {
+    int argc_value;
     int int_array[argc - 1];
     int i;
+    int j;
 
+    argc_value = argc - 1;
     i = 1;
-    while (argc-- > 1)
-    {
-        int_array[argc - 1] = ft_atoi(argv[i++]);
-//        printf("%d ", int_array[argc - 1]);
-    }
-//    printf("\n");
+    j = -1;
+    while (++j < argc_value)
+        int_array[j] = ft_atoi(argv[i++]);
 
-    swap_a(&int_array[4]);
-
-
+    int *sa = swap_a(&(*int_array));
+    printf("*sa = %d | sa[0] = %d | *(sa + 1) = %d | sa[1] = %d\n", *sa, sa[0], *(sa + 1), sa[1]);
+    
     return (0);
 }
-
-
--- push_swap.h
+++ push_swap.h
@@ -1,5 +1,10 @@
 # include <stdio.h>
 # include <unistd.h>
 
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
 int	ft_atoi(const char *str);
-int swap_a(int a[]);
+int *swap_a(int *a);
+
+#endif
